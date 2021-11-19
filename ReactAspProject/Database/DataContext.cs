0da823b7263dc3792daba4ec6468092042f252e0
LinNes <lnestorson@gmail.com>
2021-11-19 14:39:50 +0100

    Add project files.

---
+++ ReactAspProject/Database/DataContext.cs
@@ -0,0 +1,17 @@
+﻿namespace ReactAspProject.Database
+{
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Activity> Activities { get; set; }
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
